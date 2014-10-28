a440e63d79754c20a05d379b8e294b8c2a595219
Brady Erickson <brady.erickson@polaris.com>
2014-10-28 06:59:31 -0500

    Added translations and more directions for app config

--- src/main/webapp/scripts/pages/default/default-page-controller.js
+++ src/main/webapp/scripts/pages/default/default-page-controller.js
@@ -1,5 +1,8 @@
-function DefaultPageController($scope, strTitle) {
+function DefaultPageController($scope, strTitle, translation) {
 
-    $scope.pageTitle = strTitle;
+    // We would normally use the translate directive (e.g. <h2 translate>Text to translate</h2>).  But, it is
+    // possible to use the service instead and translate the content before putting it on scope like below.
+    var translatedTitle = translation.getString(strTitle);
+    $scope.pageTitle = translatedTitle;
 
 }
--- src/main/webapp/scripts/pages/default/default-page-module.js
+++ src/main/webapp/scripts/pages/default/default-page-module.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('appname.pages.defaultpage', ['polaris.directives.dexHeader', 'polaris.directives.dexFooter', 'polaris.directives.tabs'])
+    angular.module('appname.pages.defaultpage', ['polaris.services.translation'])
         .constant('strTitle', 'Polaris Angular App Page Title')
-        .controller('defaultPageController', ['$scope', 'strTitle', DefaultPageController]);
+        .controller('defaultPageController', ['$scope', 'strTitle', 'translation', DefaultPageController]);
 })();
